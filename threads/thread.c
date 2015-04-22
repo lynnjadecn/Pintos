6e6a30a4fe3e2655fdf95645a57e5a5c50a64828
linzhangwei <lynnjadecn@gmail.com>
2015-04-22 13:26:35 +0800

    finish real time priority

--- threads/thread.c
+++ threads/thread.c
@@ -212,6 +212,9 @@ thread_create (const char *name, int priority,
   /* Add to run queue. */
   thread_unblock (t);
 
+  if(priority > thread_current()->priority)
+    thread_yield();
+
   return tid;
 }
 
@@ -348,6 +351,11 @@ void
 thread_set_priority (int new_priority) 
 {
   thread_current ()->priority = new_priority;
+  if (!list_empty(&ready_list)
+                  && list_entry(list_begin(&ready_list), struct thread,
+                          elem) -> priority > new_priority)
+    thread_yield();
+
 }
 
 /* Returns the current thread's priority. */
